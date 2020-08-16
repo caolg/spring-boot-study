ae5b3550a8272ad3286e6e28a8c0a071a16081ab
caolg <caoliangang123@163.com>
2020-08-16 21:50:51 +0800

    spring boot demo-0816 first

---
+++ demo-0816/src/main/java/com/demo01/Demo0816Application.java
@@ -0,0 +1,13 @@
+package com.demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0816Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0816Application.class, args);
+    }
+
+}
